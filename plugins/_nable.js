69cbd854b31567053266b906ef7af137dee66823
daysite <danieldevelop3@gmail.com>
2025-12-17 00:38:59 -0500

    Update _nable.js

--- plugins/_nable.js
+++ plugins/_nable.js
@@ -15,7 +15,7 @@ let handler = async (m, { conn, usedPrefix, command, args }) => {
 
   let fkontak = { key: { participants: "0@s.whatsapp.net", remoteJid: "status@broadcast", fromMe: false, id: "Halo" }, message: { contactMessage: { vcard: `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` } }, participant: "0@s.whatsapp.net" }
 
-  const imageUrl = "https://iili.io/Ka6T0Xf.jpg"
+  const imageUrl = "https://files.catbox.moe/n4npwx.jpg"
   let imageBuffer = await fetch(imageUrl).then(res => res.buffer())
 
 const listMessage = {
